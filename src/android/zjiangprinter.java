53fece7576f139e9b4c8d520f1164e41ac1abefe
iffifan <irfanamir@live.com>
2015-04-25 06:55:55 +0500

    initial test

--- src/android/zjiangprinter.java
+++ src/android/zjiangprinter.java
@@ -9,18 +9,28 @@ public class zjiangprinter extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
-        if (action.equals("greet")) {
+        if (action.equals("list")) {
 
-            String name = data.getString(0);
-            String message = "Hello, " + name;
-            callbackContext.success(message);
-
+            listBT(callbackContext);
             return true;
 
         } else {
             
             return false;
 
+        }
+    }
+
+    void listBT(CallbackContext callbackContext) {
+
+        try {
+            String message = "Hello, Worldy";
+            callbackContext.success(message);
+
+        } catch (Exception e) {
+            errMsg = e.getMessage();
+            Log.e(LOG_TAG, errMsg);
+            callbackContext.error(errMsg);
         }
     }
 }
--- www/zjiangprinter.js
+++ www/zjiangprinter.js
@@ -1,7 +1,7 @@
 /*global cordova, module*/
 
 module.exports = {
-    greet: function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "zjiangprinter", "greet", [name]);
+    list: function (name, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "zjiangprinter", "list", [name]);
     }
 };
